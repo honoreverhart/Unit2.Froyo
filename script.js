54c9ccff6526e24f7b53320d4403c869fa65fd28
Honor Everhart <honoreverhart@gmail.com>
2025-01-16 14:56:36 -0700

    Workshop 15 Froyo updates

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
 let userFlavors = prompt("Enter Your Order");
 
 const froyo = {
-    chocolate: 0,
-    vanilla: 0,
-    strawberry: 0,
-    coffee: 0,
+  chocolate: 0,
+  vanilla: 0,
+  strawberry: 0,
+  coffee: 0,
 };
 
-const orderArray = userFlavors.split(",")
+console.log(froyo);
 
+function froyoFlavors() {
+  const order = userFlavors.split(",").map(flavor => flavor.trim());
 
-
-
-for (let index = 0; index < orderArray.length; index++) {
-    if (orderArray[index] == "chocolate") {
-       froyo.chocolate ++
-    } else if (orderArray[index] == "strawberry"){
-        froyo.strawberry ++
-    }else if (orderArray[index] == "vanilla"){
-        froyo.vanilla ++
-    }else if (orderArray[index] == "coffee"){
-        froyo.coffee ++
+  order.forEach((flavor) => {
+    if (froyo[flavor] !== undefined) {
+      froyo[flavor]++;
+    } else {
+      froyo[flavor] = 1;
     }
+  });
+
+  console.log(froyo);
+
+  // console.table(froyo.flavors);
+  //   const orderArray = userFlavors.split(",");
+  //   for (let index = 0; index < orderArray.length; index++) {
+  //     if (orderArray[index] == "chocolate") {
+  //       froyo.chocolate++;
+  //     } else if (orderArray[index] == "strawberry") {
+  //       froyo.strawberry++;
+  //     } else if (orderArray[index] == "vanilla") {
+  //       froyo.vanilla++;
+  //     } else if (orderArray[index] == "coffee") {
+  //       froyo.coffee++;
+  //     }
+
+  //   }
+  //   console.table(froyo);
+  //   addFlavor();
 }
 
 
+froyoFlavors();
 
 
-console.table(froyo)
-
-//Other Option
-
-// const order = userResponse.split(",");
-
-// order.forEach(flavor => {
-//     if (froyo.flavors[flavor]){
-//         froyo.flavors[flavor]++
-//     }else {
-//         froyo.flavors[flavor] = 1
-//     }
-// })
-
-// console.table(froyo.flavors);
